59bc957ce3d46a5e04079cd5621073150232b631
Tran Toan <toantpm@var-meta.com>
2024-10-23 21:57:50 +0700

    Update generate-data.js

--- generate-data.js
+++ generate-data.js
@@ -1,41 +1,83 @@
 import { faker } from "@faker-js/faker";
 import fs from "fs";
-faker.location = {
-   country: "Vietnam",
-};
-
-const generateUsers = (amount) => {
+const generateRandomUser = (count) => {
    const users = [];
 
-   for (let index = 0; index < amount; index++) {
-      const randomUser = {
+   for (let index = 0; index < count; index++) {
+      const user = {
          id: faker.string.uuid(),
-         name: faker.person.fullName(),
+         firstName: faker.person.firstName(),
+         lastName: faker.person.lastName(),
          email: faker.internet.email(),
-         phone: faker.phone.imei(),
+         avatar: faker.image.avatar(),
+         jobTitle: faker.person.jobTitle(),
+         phone: faker.phone.number(),
          company: faker.company.name(),
          website: faker.internet.url(),
       };
-      users.push(randomUser);
+
+      users.push(user);
    }
 
    return users;
 };
 
-const generateDate = () => {
-   /** Khởi tạo data mẫu */
-   const users = generateUsers(10);
+const generateRandomCategories = (count) => {
+   const categories = [];
 
-   /** Tạo các object chứa data mẫu để lưu vào db.json */
-   const db = {
+   for (let index = 0; index < count; index++) {
+      const category = {
+         id: faker.string.uuid(),
+         name: faker.commerce.department(),
+         image: faker.image.urlLoremFlickr(),
+      };
+
+      categories.push(category);
+   }
+
+   return categories;
+};
+
+const generateRandomProducts = (categories, count) => {
+   const products = [];
+
+   for (let index = 0; index < categories.length; index++) {
+      const element = categories[index];
+
+      for (let i = 0; i < count; i++) {
+         const product = {
+            categoryId: element.id,
+            id: faker.string.uuid(),
+            name: faker.commerce.productName(),
+            price: faker.commerce.price(),
+            image: faker.image.urlLoremFlickr(),
+            quality: faker.number.int({ max: 100 }),
+         };
+         products.push(product);
+      }
+   }
+
+   return products;
+};
+
+const generateData = () => {
+   /** Tạo data mẫu */
+   const users = generateRandomUser(50);
+   const categories = generateRandomCategories(5);
+   const products = generateRandomProducts(categories, 10);
+
+   /** Tạo object chứa bảng database */
+   const object = {
       users: users,
-      /** Có nhiều data hơn thì mình sẽ để ở dưới này */
+      categories: categories,
+      products: products,
+      profile: {},
    };
 
-   /** Lưu data vào db.json */
-   fs.writeFile("db.json", JSON.stringify(db), () => {
-      console.log("Save to database json server successfully!!");
+   /** lưu object data vào database */
+   fs.writeFile("db.json", JSON.stringify(object), () => {
+      console.log("Lưu data thành công");
    });
 };
 
-generateDate();
+generateData();
