587d6ca8cddebe4c227de82f789fe2b00fb5dee2
thanhtamnguyen-th77 <thanhtam.th77@gmail.com>
2024-11-13 19:41:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,6 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router);
-server.listen(5000, () => {
+server.listen(3000, () => {
    console.log("JSON Server is running");
 });
